3917a8f2294f5ee4544fd773962529196bdcab29
김영은 <95126m@naver.com>
2025-02-03 16:05:20 +0900

    fix

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Spinner from '@/assets/loading.gif'
 
 export const Loading = () => {
--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [
     react({
-      jsxImportSource: '@emotion/react', 
-    }),
+      jsxImportSource: '@emotion/react' // ✅ Emotion JSX 설정 유지
+    })
   ],
-  esbuild: {
-    jsxFactory: 'jsx',
-    jsxFragment: 'Fragment',
-    jsxInject: `import { jsx, css } from '@emotion/react'`, 
-    jsxImportSource: '@emotion/react'
-  },
   resolve: {
     alias: [
       { find: '@', replacement: '/src' },
